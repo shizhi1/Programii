857796f19c704fe608af4daf38bc5bea9b15afc3
Ivan-Vasil <ivanvasilgenev@abv.bg>
2012-10-08 14:12:39 +0300

    Задача 2

---
+++ Homework2/csv2.py
@@ -0,0 +1,20 @@
+import csv
+f=open('testthis.csv','r')
+reader = csv.reader(f)
+for row in reader:
+   a=0
+   b=0
+   c=0
+    
+if row[4] == "ruby" : 
+    a=a + row[3]
+if row[4] == "perl" :
+    
+    b= b + row[3]
+if row[4] == "python" :
+    c=c + row[3]
+
+    print "python ",a,""
+    print "ruby",b, ""
+    print  "perl",c,""
+    file_csv.close()
