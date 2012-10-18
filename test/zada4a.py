c8b3b1c0526b7eed07a4c170a651568f35d770ba
Ivan-Vasil <ivanvasilgenev@abv.bg>
2012-10-18 10:01:28 +0300

    Изпит по ТП
    Технологично Училище Електронни Системи
    Иван-Васил Тодоров Генев
    Номер 13
    11 А

---
+++ test/zada4a.py
@@ -0,0 +1,15 @@
+import csv
+
+with open('zada4a.csv', 'rb') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print row
+
+
+    aList = []
+
+        another = [zada4a.csv[1]]
+
+        print another
+
+                   
